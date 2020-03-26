bcea20e87f6217af7d7e0c69e23930d84ef605ea
filipemf1 <36512008+filipemf1@users.noreply.github.com>
2020-03-25 22:53:41 -0300

    Create primeiro.dart
    aula 25-03

---
+++ primeiro app/primeiro.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: HomePage
+    body: Text("TAI 2020"),
+  ),
+));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: appBar(
+      title: text("TAI 7 semestre"),
+    ),
+    drawer: Drawer(
+      chil: Text("TAI"),
+    ),
+  }
+}
